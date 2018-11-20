e537edf68e8962e962688da0654d2036f6c7cdc4
mkyu0917 <mkyu0917@gmail.com>
2018-11-21 01:26:55 +0900

    에러제거

--- animal_project/src/animal_project/DBConn.java
+++ animal_project/src/animal_project/DBConn.java
@@ -8,29 +8,18 @@ public class DBConn {
 		Connection conn = null;
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver"); 
-<<<<<<< HEAD
 			
 			String url = "jdbc:mysql://localhost:3307/animaldb?characterEncoding=EUC-KR&serverTimezone=UTC&autoReconnect=true&useSSL=false&allowPublicKeyRetrieval=true";
 			String id = "root"; 		
 			String pw = "qwer1234!"; 
 
-=======
-			
-			String url = "jdbc:mysql://localhost:3306/animaldb?characterEncoding=EUC-KR&serverTimezone=UTC&autoReconnect=true&useSSL=false&allowPublicKeyRetrieval=true";
-			String id = "root"; 		// »ç¿ëÀÚ°èÁ¤
-			String pw = "plus_1004"; 	// »ç¿ëÀÚ°èÁ¤ÀÇ ÆÐ½º¿öµå 
-			
->>>>>>> branch 'master' of https://github.com/DobbyData/animal_project.git
 			conn=DriverManager.getConnection(url,id,pw);
 		}catch(SQLException e){
 			System.err.println("Error" + e.getMessage());
 			
 		} catch(ClassNotFoundException e){
-<<<<<<< HEAD
 			System.err.println("JDBC class found exception error.");
-=======
-			System.err.println("JDBC µå¶óÀÌ¹ö¸¦ Ã£Áö ¸øÇß½À´Ï´Ù.");
->>>>>>> branch 'master' of https://github.com/DobbyData/animal_project.git
+			System.err.println("JDBC ï¿½ï¿½ï¿½ï¿½Ì¹ï¿½ï¿½ï¿½ Ã£ï¿½ï¿½ ï¿½ï¿½ï¿½ß½ï¿½ï¿½Ï´ï¿½.");
 		}
 		return conn;
 	}
